3fdd57cbd3eb104c5ec573caf61c8bdfbb3bc24e
fabienfrfr <fabien.furfaro@gmail.com>
2022-08-11 16:08:08 +0200

    v0.0.3

--- setup.py
+++ setup.py
@@ -8,7 +8,7 @@ here = os.path.abspath(os.path.dirname(__file__))
 with codecs.open(os.path.join(here, "README.md"), encoding="utf-8") as fh:
     long_description = "\n" + fh.read()
 
-VERSION = '0.0.2'
+VERSION = '0.0.3'
 DESCRIPTION = 'Evolutionnary Neural Network Model with PyTorch'
 LONG_DESCRIPTION = 'A package based on the article : An Artificial Neural Network Functionalized by Evolution'
 
